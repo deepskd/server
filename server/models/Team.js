f81464db60e774ac1a57a1477e9191880d631c3c
Deepak <deepskd@gmail.com>
2018-12-06 11:37:46 -0800

    initial team model

--- server/models/Team.js
+++ server/models/Team.js
@@ -1,2 +1,18 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+
+const teamSchema = new Schema({
+  name: String,
+  mascot: String,
+  city: String,
+  state: String,
+  zip: String,
+  address: String,
+  colors: [String]
+});
+
+teamSchema.statics.findByName = function(name, cb) {
+  return this.find({ name: new RegExp(name, "i") }, cb);
+};
+
+const Team = mongoose.model("teams", teamSchema);
