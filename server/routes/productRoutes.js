dd772a3c439cbfbbff5be6b821d6d2fbdd386e44
Deepak <deepskd@gmail.com>
2019-01-04 10:55:37 -0800

    Home color v1

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -17,9 +17,9 @@ obj=a/o/cuf&src=sld_pn_white&show&\
 obj=a/o/pip&src=sld_pn_white&show&\
 obj=a/o/ufr&decal&show&res=10.053222945002956&pos=0,0&\
 src=fxg{APP18_pn1_jht_teamname?&\
-$text=EAGLES&\
+$text=TEAMNAME&\
 $font=red_zone_2015&\
-$text_color=sld_pn_collegiate_burgundy_ht&\
+$text_color=TEAMTEXTCOLOR&\
 $stroke_color=sld_pn_collegiate_gold_ht&\
 $application=heat_transfer}&
 obj=a/o/cfr&decal&show&res=10.567757977621218&pos=0,0&\
@@ -27,29 +27,94 @@ src=fxg{APP18_pn1_jht_playernumber?&\
 $application=heat_transfer&\
 $text=99&\
 $font=red_zone_2015&\
-$text_color=sld_pn_collegiate_burgundy_ht&\
+$text_color=NUMBERTEXTCOLOR&\
 $stroke_color=sld_pn_collegiate_gold_ht}&\
 obj=a/o/cba&decal&show&res=8.800690250215704&pos=0,0&\
 src=fxg{APP18_pn1_jht_playernumber?&\
 $application=heat_transfer&\
 $text=99&\
 $font=red_zone_2015&\
-$text_color=sld_pn_collegiate_burgundy_ht&\
+$text_color=NUMBERTEXTCOLOR&\
 $stroke_color=sld_pn_collegiate_gold_ht}&\
 obj=a/o/sln&decal&show&res=35.78947368421053&pos=0,0&\
 src=fxg{APP18_pn1_jht_playernumber?&\
 $application=heat_transfer&\
 $text=99&
 $font=red_zone_2015&
-$text_color=sld_pn_collegiate_burgundy_ht&\
+$text_color=NUMBERTEXTCOLOR&\
 $stroke_color=sld_pn_collegiate_gold_ht}&\
 obj=a&req=object}\
 &resMode=sharp2\
 &op_usm=1.2,1,4,0`;
 
+const colorMapHT = color => {
+  console.log(color);
+  let colorHT = "";
+  switch (color) {
+    case "black":
+      colorHT = "sld_pn_obsidian_shine_ht";
+      break;
+    case "orange":
+      colorHT = "sld_pn_collegiate_orange_ht";
+      break;
+    case "purple":
+      colorHT = "sld_pn_collegiate_purple_ht";
+      break;
+    case "maroon":
+      colorHT = "sld_pn_maroon_ht";
+      break;
+    case "navy":
+      colorHT = "sld_pn_collegiate_navy_ht";
+      break;
+    case "navy":
+      colorHT = "sld_pn_collegiate_navy_ht";
+      break;
+    case "dark green":
+      colorHT = "sld_pn_dark_green_ht";
+      break;
+    case "dark green":
+      colorHT = "sld_pn_dark_green_ht";
+      break;
+    case "burgandy":
+      colorHT = "sld_pn_collegiate_burgundy_ht";
+      break;
+    default:
+      colorHT = "sld_pn_obsidian_shine_ht";
+  }
+  console.log(colorHT);
+  return colorHT;
+};
+
+const homeColors = (home, colors) => {
+  console.log("Colors:", colors.length);
+  const color = {};
+  if (colors.length === 2) {
+    color.primary = colorMapHT(colors[0]);
+    home = _.replace(home, /(TEAM|NUMBER)TEXTCOLOR/g, color.primary);
+    color.secondary = colors[1];
+  } else if (colors.length === 3) {
+    color.primary = colors[0];
+    color.secondary = colors[1];
+    color.terinary = colors[3];
+  } else {
+    home = _.replace(
+      home,
+      /(TEAM|NUMBER)TEXTCOLOR/,
+      "sld_pn_obsidian_shine_ht"
+    );
+  }
+  return home;
+};
+
+const awayColors = (away, color) => {
+  return away;
+};
+
 const teamProducts = team => {
-  const home = _.replace(url, "EAGLES", _.toUpper(team.mascot));
-  const away = _.replace(url, "EAGLES", _.toUpper(team.name));
+  var home = _.replace(url, /TEAMNAME/, _.toUpper(team.mascot));
+  home = homeColors(home, team.colors);
+  var away = _.replace(url, /TEAMNAME/, _.toUpper(team.name));
+  away = awayColors(away, team.colors);
   return { home, away };
 };
 
