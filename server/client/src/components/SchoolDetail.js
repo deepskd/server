74fcf3312aa89db33dacaf7464f57e827e42a759
Deepak <deepskd@gmail.com>
2019-01-04 09:35:05 -0800

    working MVP of A1 Jersey

--- server/client/src/components/SchoolDetail.js
+++ server/client/src/components/SchoolDetail.js
@@ -2,52 +2,35 @@ import React from "react";
 import { connect } from "react-redux";
 
 class SchoolDetail extends React.Component {
-  state = { data: {} };
-
-  updateColors = teamProd => {
-    console.log(teamProd.team);
-
-    const data = {
-      home: {
-        name: "",
-        base: "sld_pn_black",
-        textColor: "sld_dark_green_sp",
-        textOutline: "sld_pn_white_sp"
-      },
-      away: {
-        name: "",
-        base: "sld_pn_white",
-        textColor: "sld_pn_black_sp",
-        outlineColor: "sld_dark_green_sp"
-      },
-      number: 78
-    };
-
-    data.home.name = "EAGLES";
-  };
-
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    this.updateColors(this.props.teamProducts);
-  }
   render() {
     const { team } = this.props.teamProducts || null;
+    const { products } = this.props.teamProducts || null;
     if (!team) {
       return <div>Loading</div>;
     }
+    console.log(products);
     return (
-      <div className="ui centered card">
-        <div className="content">
-          <div className="header">{team.name}</div>
+      <div className="ui fluid grid">
+        <div className="ui row centered card">
+          <div className="content">
+            <div className="header">{team.name}</div>
+            <h4 className="meta">
+              {team.city},{team.state}
+            </h4>
+          </div>
+          <div className="content">
+            {team.mascot}
+            <br />
+            {team.colors}
+          </div>
         </div>
-        <div className="content">
-          <h4 className="ui sub header">
-            {team.city},{team.state}
-          </h4>
-        </div>
-        <div className="content">
-          {team.mascot}
-          <br />
-          {team.colors}
+        <div className="ui fluid row images medium">
+          <div className="column eight wide">
+            <img src={products.home} alt={"Home Football Jersey"} />
+          </div>
+          <div className="column eight wide">
+            <img src={products.away} alt={"Away Football Jersey"} />
+          </div>
         </div>
       </div>
     );
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -4,13 +4,53 @@ const fs = require("fs");
 require("../models/Team");
 
 const Team = mongoose.model("teams");
+const url = `https://embodee.adidas.com/api2/rewrite/adidas16/is/image/adidasAG/agm?&src=ir\
+{adidasAGRender/APP18_pn1_com_1?&obj=a/f/nvr&show&\
+obj=a/m/bas&src=sld_pn_white&show&\
+obj=a/s/shg&show&\
+obj=a/o/st1_s0&show&\
+obj=a/o/st2_t0&show&\
+obj=a/o/log&src=sld_pn_black&show&\
+obj=a/o/cuf&src=sld_pn_white&show&\
+obj=a/o/pip&src=sld_pn_white&show&\
+obj=a/o/ufr&decal&show&res=10.053222945002956&pos=0,0&\
+src=fxg{APP18_pn1_jht_teamname?&\
+$text=EAGLES&\
+$font=red_zone_2015&\
+$text_color=sld_pn_collegiate_burgundy_ht&\
+$stroke_color=sld_pn_collegiate_gold_ht&\
+$application=heat_transfer}&
+obj=a/o/cfr&decal&show&res=10.567757977621218&pos=0,0&\
+src=fxg{APP18_pn1_jht_playernumber?&\
+$application=heat_transfer&\
+$text=99&\
+$font=red_zone_2015&\
+$text_color=sld_pn_collegiate_burgundy_ht&\
+$stroke_color=sld_pn_collegiate_gold_ht}&\
+obj=a/o/cba&decal&show&res=8.800690250215704&pos=0,0&\
+src=fxg{APP18_pn1_jht_playernumber?&\
+$application=heat_transfer&\
+$text=99&\
+$font=red_zone_2015&\
+$text_color=sld_pn_collegiate_burgundy_ht&\
+$stroke_color=sld_pn_collegiate_gold_ht}&\
+obj=a/o/sln&decal&show&res=35.78947368421053&pos=0,0&\
+src=fxg{APP18_pn1_jht_playernumber?&\
+$application=heat_transfer&\
+$text=99&
+$font=red_zone_2015&
+$text_color=sld_pn_collegiate_burgundy_ht&\
+$stroke_color=sld_pn_collegiate_gold_ht}&\
+obj=a&req=object}\
+&resMode=sharp2\
+&op_usm=1.2,1,4,0`;
 
-var products = {};
-try {
-  products = yaml.safeLoad(fs.readFileSync("product.yml", "utf8"));
-} catch (e) {
-  console.log(e);
-}
+var products = { home: url, away: url };
+// try {
+//   products = yaml.safeLoad(fs.readFileSync("product.yml", "utf8"));
+// } catch (e) {
+//   console.log(e);
+// }
 
 module.exports = app => {
   app.get("/api/products", async (req, res) => {
