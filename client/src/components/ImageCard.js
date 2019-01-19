1bb208386014b61370375603612c09ca9b7d15c4
Deepak <deepskd@gmail.com>
2019-01-18 16:17:56 -0800

    added dimmer

--- client/src/components/ImageCard.js
+++ client/src/components/ImageCard.js
@@ -3,17 +3,37 @@ import React from "react";
 class ImageCard extends React.Component {
   constructor(props) {
     super(props);
-
+    this.state = { loading: true };
     this.imageRef = React.createRef();
   }
 
   componentDidMount() {
-    console.log(this.imageRef);
+    this.imageRef.current.addEventListener("load", this.hideLoader);
+  }
+
+  hideLoader = () => {
+    this.setState({ loading: false });
+  };
+
+  renderLoader() {
+    let loader = "ui active inverted dimmer";
+    if (!this.state.loading) {
+      return <div />;
+    }
+    return (
+      <div className="ui segment">
+        <p />
+        <div className={loader}>
+          <div className="ui loader" />
+        </div>
+      </div>
+    );
   }
 
   render() {
     return (
       <div className="column eight wide">
+        {this.renderLoader()}
         <img ref={this.imageRef} src={this.props.src} alt={this.props.alt} />
       </div>
     );
