3a4cd58710d2997c6a1fe41afa0a3f23ac5ee2af
Deepak <deepskd@gmail.com>
2019-02-01 11:41:01 -0800

    initial Basketball Component

--- client/src/components/Basketball.js
+++ client/src/components/Basketball.js
@@ -1,9 +1,79 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import ImageCard from "./ImageCard";
+import SchoolCard from "./SchoolCard";
+import { fontChanged } from "../actions";
 
 class Basketball extends React.Component {
+  fontChanged(font) {
+    this.props.fontChanged(font);
+  }
+
+  fontOptions(fonts, selectedFont) {
+    return Object.keys(fonts).map(font => {
+      if (font === selectedFont) {
+        return (
+          <button key={font} className="positive ui button">
+            {fonts[font]}
+          </button>
+        );
+      } else {
+        return (
+          <button
+            key={font}
+            className="ui button"
+            onClick={() => this.fontChanged(font)}
+          >
+            {fonts[font]}
+          </button>
+        );
+      }
+    });
+  }
+
   render() {
-    return <div>Basketball</div>;
+    const { team } = this.props.teamProducts || null;
+    const { products } = this.props.teamProducts || null;
+    if (!team) {
+      return <div>Loading</div>;
+    }
+    return (
+      <div className="ui fluid grid">
+        <SchoolCard team={team} />
+
+        <div className="left floated fourteen wide column">
+          <div className="ui grid">
+            <div className="ui two cards">
+              <ImageCard
+                src={products.home.jersey}
+                alt={"Home Basketball Jersey"}
+              />
+              <ImageCard
+                src={products.away.jersey}
+                alt={"Away Basketball Jersey"}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="right floated two wide column">
+          Font Selector
+          <div className="ui vertical mini buttons">
+            {this.fontOptions(products.fonts, products.selectedFont)}
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
-export default Basketball;
+const mapStateToProps = state => {
+  return {
+    teamProducts: state.teamProducts
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { fontChanged }
+)(Basketball);
