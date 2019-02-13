2e52e7dcb2a2bdc93fa18cac7a8ae3c0edcb708c
Deepak <deepskd@gmail.com>
2019-02-13 09:02:42 -0800

    initial ui changes for Jersey Text

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -6,6 +6,38 @@ import SchoolCard from "./SchoolCard";
 import { fontChanged } from "../actions";
 
 class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { mascot: "", teamName: "" };
+  }
+  onTextChange(obj) {
+    const attr = Object.keys(obj)[0];
+    switch (attr) {
+      case "mascot":
+        this.setState({ mascot: obj["mascot"].target.value });
+        break;
+      case "teamName":
+        this.setState({ teamName: obj["teamName"].target.value });
+        break;
+      default:
+        console.log("should not get here");
+    }
+  }
+
+  textUpdated(element) {
+    console.log(element);
+    switch (element) {
+      case "mascot":
+        console.log(this.state.mascot);
+        break;
+      case "teamName":
+        console.log(this.state.teamName);
+        break;
+      default:
+        console.log("Should not get here");
+    }
+  }
+
   fontChanged(event) {
     this.props.fontChanged(event.target.value);
   }
@@ -29,40 +61,65 @@ class ProductList extends React.Component {
     return (
       <div className="ui fluid grid">
         <SchoolCard team={team} />
-
-        <div className="left floated fourteen wide column">
+        <div className="left floated twelve wide column">
           <div className="ui grid">
             <div className="ui two cards">
-              <ImageCard
-                src={products.home.jersey}
-                alt={"Home Football Jersey"}
-              />
-              <ImageCard
-                src={products.away.jersey}
-                alt={"Away Football Jersey"}
-              />
+              <ImageCard src={products.home.jersey} alt={"Home Jersey"} />
+              <ImageCard src={products.away.jersey} alt={"Away Jersey"} />
             </div>
             <div className="ui two cards">
-              <ImageCard
-                src={products.home.pants}
-                alt={"Home Football Pants"}
-              />
-              <ImageCard
-                src={products.away.pants}
-                alt={"Away Football Pants"}
-              />
+              <ImageCard src={products.home.pants} alt={"Home Pants"} />
+              <ImageCard src={products.away.pants} alt={"Away Pants"} />
             </div>
           </div>
         </div>
-        <div className="right floated two wide column">
-          Font Selector
-          <select
-            className="ui dropdown"
-            onChange={event => this.fontChanged(event)}
-            value={products.selectedFont}
-          >
-            {this.fontOptions(products.fonts)}
-          </select>
+        <div className="right floated four wide column">
+          <form className="ui form" onSubmit={e => e.preventDefault()}>
+            <div className="field">
+              <label>Font</label>
+              <select
+                className="ui dropdown"
+                onChange={event => this.fontChanged(event)}
+                value={products.selectedFont}
+              >
+                {this.fontOptions(products.fonts)}
+              </select>
+            </div>
+            <div className="field">
+              <label>Home Jersey Text</label>
+              <div className="ui action input">
+                <input
+                  type="text"
+                  placeholder={team.mascot}
+                  value={this.state.mascot}
+                  onChange={e => this.onTextChange({ mascot: e })}
+                />
+                <button
+                  className="ui button"
+                  onClick={event => this.textUpdated("mascot")}
+                >
+                  Update
+                </button>
+              </div>
+            </div>
+            <div className="field">
+              <label>Away Jersey Text</label>
+              <div className="ui action input">
+                <input
+                  type="text"
+                  placeholder={team.name}
+                  value={this.state.teamName}
+                  onChange={e => this.onTextChange({ teamName: e })}
+                />
+                <button
+                  className="ui button"
+                  onClick={event => this.textUpdated("teamName")}
+                >
+                  Update
+                </button>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     );
