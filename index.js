f86774496c8b4a48f995a86a38c04519c39bf572
Deepak <deepskd@gmail.com>
2019-05-29 15:02:31 -0700

    refactor passport and mongo to services

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 require("newrelic");
+require("./services/passport");
+require("./services/mongo");
 const express = require("express");
-const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const keys = require("./config/keys");
 
 require("./models/Team");
-
-mongoose.connect(
-  keys.mongoURI,
-  { useNewUrlParser: true }
-);
 
 const app = express();
 app.use(bodyParser.json());
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,21 +1,4 @@
 const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-
-const keys = require("./../config/keys");
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
-    },
-    (accessToken, refreshToken, profile) => {
-      console.log(accessToken);
-      console.log(profile);
-    }
-  )
-);
 
 module.exports = app => {
   app.get(
