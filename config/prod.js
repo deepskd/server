efe25e0674d7e972a30f320f9169263b04c0aab6
Deepak <deepskd@gmail.com>
2019-05-30 10:55:29 -0700

    adding api for current user and logout

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,6 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI
+  mongoURI: process.env.MONGO_URI,
+  googleClientID: process.env.googleClientID,
+  googleClientSecret: process.env.googleClientSecret,
+  cookieKey: process.env.cookieKey
 };
--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require("newrelic");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
 
 require("./services/mongo");
 require("./models/User");
@@ -10,6 +13,14 @@ require("./models/Team");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./routes/teamRoutes")(app);
 require("./routes/productRoutes")(app);
