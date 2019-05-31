c9114256cf4204f37b881d8e40edc651eca30149
Deepak <deepskd@gmail.com>
2019-05-31 11:17:34 -0700

    enabled redirect to home after loggin in

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,7 +6,13 @@ module.exports = app => {
     passport.authenticate("google", { scope: ["profile", "email"] })
   );
 
-  app.get("/auth/google/callback", passport.authenticate("google"));
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google"),
+    (req, res) => {
+      res.redirect("/");
+    }
+  );
 
   app.get("/api/currentUser", (req, res) => {
     res.send(req.user);
