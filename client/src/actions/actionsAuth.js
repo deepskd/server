c5a224f17a0813d7da5e7b7b7736ef2614957322
Deepak <deepskd@gmail.com>
2019-05-31 10:07:11 -0700

    add actions for fetch user

---
+++ client/src/actions/actionsAuth.js
@@ -0,0 +1,9 @@
+import gts from "../apis/gts";
+// import axios from "axios";
+
+import { FETCH_USER } from "./types.js";
+
+export const fetchUser = async dispatch => {
+  const response = await gts.get("/currentUser");
+  dispatch({ type: FETCH_USER, payload: response.data });
+};
