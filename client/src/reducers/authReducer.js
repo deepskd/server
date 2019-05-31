dbc114e861c61ffd5bb723be25a43d44e1de2404
Deepak <deepskd@gmail.com>
2019-05-31 10:52:04 -0700

    quick cleanup

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,9 +1,9 @@
 import { FETCH_USER } from "../actions/types.js";
-export default function(state = [], action) {
+
+export default function(state = null, action) {
   switch (action.type) {
     case FETCH_USER:
-      console.log(state, action);
-      return state;
+      return action.payload || false;
     default:
       return state;
   }
