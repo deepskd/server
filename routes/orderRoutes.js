9dc2d40f6176e8e117823466bb33ef3dfd2275d5
Deepak Krishnakumar <deepskd@gmail.com>
2019-06-07 14:54:23 -0700

    added route to assign orders

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,12 +1,22 @@
 require("../models/Order");
 const mongoose = require("mongoose");
+const requireLogin = require("../middlewares/requireLogin");
 
 const Order = mongoose.model("orders");
 
 module.exports = app => {
-  app.get("/api/orders", async (req, res) => {
+  app.get("/api/orders", requireLogin, async (req, res) => {
     const orders = await Order.findByOrderName(req.query.orderName);
 
     res.status(200).send(orders);
+  });
+
+  app.patch("/api/orders", requireLogin, async (req, res) => {
+    const updates = await Image.assignTeamToImages(
+      req.body.selectedOrderIds,
+      req.body.teamId
+    );
+
+    res.status(200).send({ message: "Records Updated", meta: updates });
   });
 };
