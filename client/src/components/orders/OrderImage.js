761e6ddda54a89e62cdf52546194ac85ce13ae74
Deepak Krishnakumar <deepskd@gmail.com>
2019-06-27 16:05:19 -0700

    modal for image views

---
+++ client/src/components/orders/OrderImage.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import { Modal, Image } from 'semantic-ui-react'
+import _ from 'lodash'
+
+class OrderImage extends Component {
+  state = { modalOpen: false }
+
+  handleOpen = () => this.setState({ modalOpen: true })
+
+  handleClose = () => this.setState({ modalOpen: false })
+  render() {
+    const frontImage = this.props.src
+
+    const img1 = _.chain(frontImage).replace(/&wid=201$/, '')
+    const img4 = _.chain(img1).replace(/_\d/, '_4')
+    const img8 = _.chain(img1).replace(/_\d/, '_8')
+    const img12 = _.chain(img1).replace(/_\d/, '_12')
+
+    return (
+      <Modal
+        trigger={<Image src={frontImage} onClick={this.handleOpen} />}
+        open={this.state.modalOpen}
+        onClose={this.handleClose}
+        size="large"
+      >
+        <Modal.Content image>
+          <Image wrapped size="large" src={img1} />
+          <Image wrapped size="large" src={img4} />
+          <Image wrapped size="large" src={img8} />
+          <Image wrapped size="large" src={img12} />
+        </Modal.Content>
+      </Modal>
+    )
+  }
+}
+
+export default OrderImage
