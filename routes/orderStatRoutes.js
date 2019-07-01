f933129cbc43745fb5d915268c79f2ebeae3a364
Deepak Krishnakumar <deepskd@gmail.com>
2019-07-01 14:23:09 -0700

    server model route and index

---
+++ routes/orderStatRoutes.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const OrderStat = mongoose.model('orderstats')
+const requireLogin = require('../middlewares/requireLogin')
+
+module.exports = app => {
+  app.get('/api/orderStats', requireLogin, async (req, res) => {
+    let { query } = req
+    if (query.retailerType) {
+      query.retailerType = query.retailerType.split('|')
+    } else {
+      query.retailerType = ['Wholesale', 'Promotion']
+    }
+    let stats = await OrderStat.findByCountry(query)
+    stats.query = query
+    res.send(stats)
+  })
+}
