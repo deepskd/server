950c77633120b4321b67821b304dab2083b3a01c
Deepak Krishnakumar <deepskd@gmail.com>
2019-07-02 10:14:29 -0700

    added option to save  user data

--- models/User.js
+++ models/User.js
@@ -3,6 +3,18 @@ const { Schema } = mongoose
 
 const User = new Schema({
   googleId: String,
+  displayName: String,
+  email: String,
+  createdOn: { type: Date, default: Date.now },
+  lastLogIn: { type: Date, default: Date.now },
+})
+
+User.pre('save', function(next) {
+  now = new Date()
+  this.lastLogIn = now
+  if (!this.createdOn) {
+    this.createdOn = now
+  }
 })
 
 mongoose.model('users', User)
