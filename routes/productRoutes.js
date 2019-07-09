8ca2d91faff3f19abcef19c9e73472b009759cb5
Deepak Krishnakumar <deepskd@gmail.com>
2019-07-09 14:47:52 -0700

    added product route for volleyball

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,6 +5,7 @@ require('../models/Team')
 const a1PrimeKnitUniform = require('../uniforms/a1Primeknit')
 // const tripleUp = require('../uniforms/tripleUp')
 const reign = require('../uniforms/reign')
+const volleyball17 = require('../uniforms/volleyball17')
 
 const Team = mongoose.model('teams')
 
@@ -140,7 +141,7 @@ const basketball = team => {
 
   jersey.articleDescription = 'Reign Jersey'
   jersey.price = '$78'
-  pant.articleDescription = 'Reign Pant'
+  pant.articleDescription = 'Reign Shorts'
   pant.price = '$77'
 
   jersey.frontText = _.toUpper(mascot)
@@ -199,7 +200,7 @@ const basketball = team => {
 
   jersey.articleDescription = 'Reign Jersey'
   jersey.price = '$78'
-  pant.articleDescription = 'Reign Pant'
+  pant.articleDescription = 'Reign Shorts'
   pant.price = '$77'
 
   jersey.frontText = _.toUpper(_.replace(team.name, '/', ' '))
@@ -263,12 +264,150 @@ const basketball = team => {
   }
 }
 
+const volleyball = team => {
+  const playerNumber = _.random(0, 99)
+  const font = _.sample(Object.keys(volleyball17.FONTS))
+  const mascot = _.replace(team.mascot || team.name, '/', ' ')
+
+  let home = {},
+    jersey = {},
+    pant = {}
+
+  jersey.articleDescription = 'Volleyball 17 Jersey'
+  jersey.price = '$70'
+  pant.articleDescription = 'Volleyball 17 Shorts'
+  pant.price = '$40'
+
+  jersey.frontText = _.toUpper(mascot)
+  jersey.baseImageURL = volleyball17.JERSEY_URL
+
+  jersey.baseColor = 'white'
+  jersey.baseColorCode = volleyball17.colorMap(jersey.baseColor)
+  jersey.baseColorHex =
+    volleyball17.BASEOPTIONS.jersey[jersey.baseColorCode].hex
+
+  jersey.logoColor = team.colors ? team.colors[0] : 'black'
+  jersey.logoColorCode = volleyball17.colorMap(jersey.logoColor)
+
+  jersey.teamTextColor = team.colors ? team.colors[0] : 'black'
+  jersey.teamTextColorCode = volleyball17.colorMap(jersey.teamTextColor)
+
+  jersey.teamStrokeColor = team.colors ? team.colors[1] : 'black'
+  jersey.teamStrokeColorCode = volleyball17.colorMap(jersey.teamStrokeColor)
+
+  jersey.font = font
+
+  jersey.frontImage = _.chain(jersey.baseImageURL)
+    .replace(/TEAMNAME/g, jersey.frontText)
+    .replace(/BASECOLOR/, jersey.baseColorCode)
+    .replace(/LOGOCOLOR/, jersey.logoColorCode)
+    .replace(/PLAYERNUMBER/g, playerNumber)
+    .replace(/(TEAM|NUMBER)FONT/g, jersey.font)
+    .replace(/TEAMTEXTCOLOR/g, jersey.teamTextColorCode)
+    .replace(/TEAMSTROKECOLOR/g, jersey.teamStrokeColorCode)
+    .value()
+  pant.baseImageURL = volleyball17.PANTS_URL
+  pant.baseColor = 'white'
+  pant.baseColorCode = volleyball17.colorMap(pant.baseColor)
+  pant.baseColorHex = volleyball17.BASEOPTIONS.pant[pant.baseColorCode].hex
+
+  pant.logoColor = team.colors ? team.colors[0] : 'black'
+  pant.logoColorCode = volleyball17.colorMap(pant.logoColor)
+
+  pant.teamTextColor = team.colors ? team.colors[0] : 'black'
+  pant.teamTextColorCode = volleyball17.colorMap(pant.teamTextColor)
+
+  pant.teamStrokeColor = team.colors ? team.colors[1] : 'black'
+  pant.teamStrokeColorCode = volleyball17.colorMap(pant.teamStrokeColor)
+
+  pant.frontImage = _.chain(pant.baseImageURL)
+    .replace(/BASECOLOR/, pant.baseColorCode)
+    .replace(/LOGOCOLOR/, pant.logoColorCode)
+    .replace(/TEAMTEXTCOLOR/g, pant.teamTextColorCode)
+    .replace(/TEAMSTROKECOLOR/, pant.teamStrokeColorCode)
+    .value()
+
+  home = volleyball17.homeDecorations({ jersey, pant }, team.colors)
+
+  let away = {}
+  jersey = {}
+  pant = {}
+
+  jersey.articleDescription = 'Volleyball 17 Jersey'
+  jersey.price = '$70'
+  pant.articleDescription = 'Volleyball 17 Shorts'
+  pant.price = '$40'
+
+  jersey.frontText = _.toUpper(_.replace(team.name, '/', ' '))
+  jersey.baseImageURL = volleyball17.JERSEY_URL
+
+  jersey.baseColor = team.colors ? team.colors[0] : 'black'
+  jersey.baseColorCode = volleyball17.colorMap(jersey.baseColor)
+  jersey.baseColorHex =
+    volleyball17.BASEOPTIONS.jersey[jersey.baseColorCode].hex
+
+  jersey.logoColor = team.colors ? team.colors[1] : 'white'
+  jersey.logoColorCode = volleyball17.colorMap(jersey.logoColor)
+
+  jersey.teamTextColor = team.colors ? team.colors[1] : 'white'
+  jersey.teamTextColorCode = volleyball17.colorMap(jersey.teamTextColor)
+
+  jersey.teamStrokeColor = team.colors ? team.colors[0] : 'white'
+  jersey.teamStrokeColorCode = volleyball17.colorMap(jersey.teamStrokeColor)
+
+  jersey.font = font
+
+  jersey.frontImage = _.chain(jersey.baseImageURL)
+    .replace(/TEAMNAME/g, jersey.frontText)
+    .replace(/BASECOLOR/, jersey.baseColorCode)
+    .replace(/LOGOCOLOR/, jersey.logoColorCode)
+    .replace(/PLAYERNUMBER/g, playerNumber)
+    .replace(/(TEAM|NUMBER)FONT/g, jersey.font)
+    .replace(/TEAMTEXTCOLOR/g, jersey.teamTextColorCode)
+    .replace(/TEAMSTROKECOLOR/g, jersey.teamStrokeColorCode)
+    .value()
+
+  pant.baseImageURL = volleyball17.PANTS_URL
+
+  pant.baseColor = team.colors ? team.colors[0] : 'black'
+  pant.baseColorCode = volleyball17.colorMap(pant.baseColor)
+  pant.baseColorHex = volleyball17.BASEOPTIONS.pant[pant.baseColorCode].hex
+
+  pant.logoColor = team.colors ? team.colors[1] : 'white'
+  pant.logoColorCode = volleyball17.colorMap(pant.logoColor)
+
+  pant.teamTextColor = team.colors ? team.colors[1] : 'white'
+  pant.teamTextColorCode = volleyball17.colorMap(pant.teamTextColor)
+
+  pant.teamStrokeColor = team.colors ? team.colors[0] : 'white'
+  pant.teamStrokeColorCode = volleyball17.colorMap(pant.teamStrokeColor)
+
+  pant.frontImage = _.chain(pant.baseImageURL)
+    .replace(/BASECOLOR/, pant.baseColorCode)
+    .replace(/LOGOCOLOR/, pant.logoColorCode)
+    .replace(/TEAMTEXTCOLOR/g, pant.teamTextColorCode)
+    .replace(/TEAMSTROKECOLOR/, pant.teamStrokeColorCode)
+    .value()
+  away = volleyball17.awayDecorations({ jersey, pant }, team.colors)
+
+  return {
+    home,
+    away,
+    fonts: volleyball17.FONTS,
+    baseOptions: volleyball17.BASEOPTIONS,
+    selectedFont: font,
+    colors: volleyball17.COLORS,
+  }
+}
+
 module.exports = app => {
   app.get('/api/products', async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
     const team = await Team.getTeam(req.query.id)
     if (req.query.sports === 'basketball') {
       res.status(200).json(basketball(team))
+    } else if (req.query.sports === 'volleyball') {
+      res.json(volleyball(team))
     } else {
       const { embellishmentMethod } = req.query
       res.status(200).json(football(team, embellishmentMethod))
