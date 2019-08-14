e518123256ecf21f88b79b816088c641f703fede
Deepak Krishnakumar <deepskd@gmail.com>
2019-08-14 14:58:19 -0700

    inital teams component

---
+++ client/src/components/teams/Teams.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Grid } from 'semantic-ui-react'
+
+const Teams = () => {
+  return (
+    <Grid>
+      <Grid.Row>Teams Page</Grid.Row>
+    </Grid>
+  )
+}
+
+export default Teams
---
+++ client/src/components/teams/TeamsNew.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Form, Grid } from 'semantic-ui-react'
+
+const TeamsNew = () => {
+  return (
+    <Grid>
+      <Form>New Team Form</Form>
+    </Grid>
+  )
+}
+
+export default TeamsNew
