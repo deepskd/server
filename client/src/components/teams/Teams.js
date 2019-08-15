9173ebbf7c261d6ef92b2e2d91d260171cfa55fc
Deepak Krishnakumar <deepskd@gmail.com>
2019-08-15 14:45:31 -0700

    basic team list

--- client/src/components/teams/Teams.js
+++ client/src/components/teams/Teams.js
@@ -1,17 +1,43 @@
 import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { Grid } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Grid, List, Icon } from 'semantic-ui-react'
 import { getTeams } from '../../actions/actionsTeam'
+import TeamItem from '../TeamItem'
 
 const Teams = () => {
   const dispatch = useDispatch()
+  const teams = useSelector(state => state.teams)
 
   useEffect(() => {
     dispatch(getTeams())
-  }, [])
+  }, [dispatch])
+
+  const renderTeams = () => {
+    if (teams.length === 0) {
+      return null
+    }
+
+    return teams.map(team => <TeamItem key={team._id} team={team} />)
+  }
+
   return (
-    <Grid>
-      <Grid.Row>Teams Page</Grid.Row>
+    <Grid columns={1}>
+      <Grid.Row>
+        <Grid.Column floated="right" width={5}>
+          <Link to="/teams/new">
+            <Icon name="plus" color="blue" size="large" />
+            Add Team
+          </Link>
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row centered>
+        <Grid.Column width={10}>
+          <List divided relaxed>
+            {renderTeams()}
+          </List>
+        </Grid.Column>
+      </Grid.Row>
     </Grid>
   )
 }
