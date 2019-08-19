51b8a8a7e9ece3b4658dbe764b53f4ae42388d0e
Deepak Krishnakumar <deepskd@gmail.com>
2019-08-19 16:09:42 -0700

    adding basic team edit page

--- client/src/actions/actionsTeam.js
+++ client/src/actions/actionsTeam.js
@@ -1,6 +1,6 @@
 import gts from '../apis/gts'
 
-import { CREATE_TEAM, GET_TEAMS } from './types'
+import { CREATE_TEAM, GET_TEAMS, GET_TEAM } from './types'
 
 export const createTeam = (team, history) => async dispatch => {
   const res = await gts.post('/teams', team)
@@ -8,8 +8,12 @@ export const createTeam = (team, history) => async dispatch => {
   dispatch({ type: CREATE_TEAM, payload: res })
 }
 
-export const getTeams = id => async dispatch => {
-  let endpoint = id ? `/v2/teams/${id}` : `/v2/teams`
-  const res = await gts.get(endpoint)
+export const getTeams = () => async dispatch => {
+  const res = await gts.get('/v2/teams')
   dispatch({ type: GET_TEAMS, payload: res })
+}
+
+export const getTeam = id => async dispatch => {
+  const res = await gts.get(`/v2/teams/${id}`)
+  dispatch({ type: GET_TEAM, payload: res })
 }
--- client/src/components/teams/TeamsEdit.js
+++ client/src/components/teams/TeamsEdit.js
@@ -1,14 +1,30 @@
-import React, { useEffect } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import { Segment } from 'semantic-ui-react'
-import { getTeams } from '../../actions/actionsTeam'
-import { useDispatch } from 'react-redux'
+import { getTeam } from '../../actions/actionsTeam'
+import { useDispatch, useSelector } from 'react-redux'
 
 const TeamEdit = ({ match }) => {
   const dispatch = useDispatch()
   useEffect(() => {
-    dispatch(getTeams(match.params.id))
-  }, [match, dispatch])
-  return <Segment>Team Edit Page</Segment>
+    dispatch(getTeam(match.params.id))
+  }, [match.params.id, dispatch])
+
+  const teams = useSelector(state => state.teams)
+  console.log(teams)
+
+  const renderTeamDetails = () => {
+    if (teams.length !== 1) {
+      return <Fragment />
+    }
+
+    let team = teams[0]
+    return (
+      <Segment>
+        {team.name}, {team.mascot}
+      </Segment>
+    )
+  }
+  return <Segment>{renderTeamDetails()}</Segment>
 }
 
 export default TeamEdit
