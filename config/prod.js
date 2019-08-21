2d076cfa9da8c5e77b6a449ed69d623b55ce8556
Deepak Krishnakumar <deepskd@gmail.com>
2019-08-21 08:48:00 -0700

    add cloudinary keys

--- config/prod.js
+++ config/prod.js
@@ -3,4 +3,7 @@ module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   cookieKey: process.env.COOKIE_KEY,
+  cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME,
+  cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
+  cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET,
 }
